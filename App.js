603a252762f9bccc8b13b9b18303f83901f3645a
biitcodemuskanshah <muskan.shah@biitcode.com>
2024-12-17 19:15:19 +0530

    push code

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import LoginScreen from './src/Screens/Authentication_module/LoginScreen'
+
+const App = () => {
+  return (
+    <LoginScreen/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
