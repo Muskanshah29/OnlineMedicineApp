26d9044bcc5571fd10fe27f3d7716dfc36ad7189
Muskanshah29 <shahmuskan29072001@gmail.com>
2025-01-10 12:22:14 +0530

    push code

--- App.js
+++ App.js
@@ -1,9 +1,7 @@
 
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import ListingScreen from './src/Screens/Authentication_module/ListingScreen'
 import StackNavigator from './src/Navigation/StackNavigator'
-import YourCart_Screen from './src/Screens/Home_module/YourCart_Screen'
 
 const App = () => {
   return (
